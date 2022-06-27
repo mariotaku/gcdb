38048b5c8f98719c7bf888aed4920f9ddb1deee6
Mariotaku <mariotaku.lee@gmail.com>
2022-06-27 12:18:49 +0900

    updating generation flow

---
+++ searchlist.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+import json
+import os
+
+import regex as re
+
+from updater import database
+
+
+def to_item(row):
+    name, image_id = row
+    key = re.sub(r'[^\p{L}\p{N}]+', '.', str(name).lower()).strip('.')
+    return key, image_id, name
+
+
+rows = sorted(map(to_item, database.execute('SELECT name, image_id FROM games '
+                                            'INNER JOIN covers c on games.id = c.game')),
+              key=lambda item: item[0])
+buckets = {}
+
+for (key, image_id, name) in rows:
+    if not len(key):
+        continue
+    bucket = key.split('.')[0][:2]
+    if not re.fullmatch(r'[\da-z]+', bucket):
+        bucket = '@'
+    items = buckets.get(bucket, None)
+    if not items:
+        items = {}
+        buckets[bucket] = items
+    items[key] = {'hash': image_id, 'name': name}
+
+os.makedirs('out/buckets', exist_ok=True)
+
+for bucket in buckets:
+    with open(f'out/buckets/{bucket}.json', mode='w', encoding='utf-8') as f:
+        json.dump(buckets[bucket], f)
---
+++ updater.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+from concurrent.futures import ThreadPoolExecutor
+from os import path
+from packaging import version
+import requests
+
+from api import Client
+from db import Database, Schema
+
+client = Client()
+schema = Schema(2, {
+    1: 'migrations/1_base.sql',
+    2: 'migrations/2_updated_at.sql'
+})
+database = Database('out/gcdb.sqlite3', schema)
+
+
+def fetch_games():
+    updated_at, = database.execute('SELECT MAX(updated_at) FROM games').fetchone()
+    begin = None
+    if not updated_at:
+        updated_at = 0
+    while True:
+        if begin:
+            print(f'Fetching games after {begin}.')
+        else:
+            print(f'Fetching games (first page)')
+        games = client.get_games(begin, updated_at=updated_at)
+        if len(games) == 0:
+            break
+        values = list(map(lambda game: (game.id, game.name, game.cover, game.updated_at), games))
+        cur = database.cursor()
+        cur.executemany('''
+INSERT INTO games(id, name, cover, updated_at)
+VALUES (?, ?, ?, ?)
+ON CONFLICT DO UPDATE SET name=excluded.name,
+                          cover=excluded.cover,
+                          updated_at=excluded.updated_at
+WHERE id = excluded.id'''.strip(), values)
+        database.commit()
+        begin = games[-1].id
+
+
+def fetch_covers():
+    begin = 0
+    while True:
+        if begin:
+            print(f'Fetching covers after {begin}.')
+        else:
+            print(f'Fetching covers (first page)')
+        rows = database.execute(f'SELECT id, cover FROM games WHERE id > {begin} AND '
+                                f'id NOT IN(SELECT game FROM covers) '
+                                f'ORDER BY id LIMIT 500').fetchall()
+        if len(rows) == 0:
+            break
+        covers = client.get_covers(map(lambda row: str(row[1]), rows))
+        values = list(map(lambda cover: (cover.id, cover.game, cover.width, cover.height, cover.image_id), covers))
+        cur = database.cursor()
+        cur.executemany('INSERT INTO covers(id, game, width, height, image_id) VALUES (?, ?, ?, ?, ?)', values)
+        database.commit()
+        begin = int(rows[-1][0])
+
+
+def fetch_assets():
+    executor = ThreadPoolExecutor(40)
+
+    def download_image(row: (str,)):
+        img, = row
+        if not path.exists(f'assets/{img}.png'):
+            with open(f'assets/{img}.png', 'wb') as f:
+                with requests.get(f'https://images.igdb.com/igdb/image/upload/t_cover_big/{img}.png') as resp:
+                    if resp.ok:
+                        f.write(resp.content)
+        if not path.exists(f'assets/{img}@2x.png'):
+            with open(f'assets/{img}@2x.png', 'wb') as f:
+                with requests.get(f'https://images.igdb.com/igdb/image/upload/t_cover_big_2x/{img}.png') as resp:
+                    if resp.ok:
+                        f.write(resp.content)
+
+    try:
+        executor.map(download_image,
+                     database.execute('SELECT image_id FROM covers WHERE width > 0 AND height > 0').fetchall())
+    finally:
+        executor.shutdown()
+
+
+if __name__ == '__main__':
+    v, = database.execute('SELECT sqlite_version()').fetchone()
+    if version.parse(v) < version.parse('3.24'):
+        print(f'SQLite 3.24 and above is required! (You have {v})')
+        exit(1)
+    fetch_games()
+    fetch_covers()
